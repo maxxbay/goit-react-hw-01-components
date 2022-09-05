49f1bca6b29bc9250348edb1d496835a1be235ab
Maxxbay <maxxbay@gmail.com>
2022-09-05 23:25:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 
-import { Profile } from '../components/Profile/Profile.jsx';
-import { Statistics } from '../components/Statistics/Statistics.jsx';
-import { FriendList } from '../components/FriendList/FriendList.jsx';
-import { TransactionHistory } from '../components/TransactionHistory/TransactionHistory.jsx';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
